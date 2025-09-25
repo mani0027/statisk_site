56987a79b0d327ae98b5d1cf9392a39e537b4b73
mani0027 <mani0027@stud.kea.dk>
2025-09-25 12:33:52 +0200

    dynamiskSingle_done

---
+++ produkt.js
@@ -0,0 +1,34 @@
+console.log("side loaded");
+
+const id = 1525;
+const productUrl = "https://kea-alt-del.dk/t7/api/products/" + id;
+const produkt = document.querySelector(".produkt");
+
+console.log("produkt:", productUrl);
+
+//Henter data ind
+
+function getData() {
+  fetch(productUrl).then((res) => res.json().then((data) => show(data)));
+}
+
+getData();
+
+function show(data) {
+  console.log("shows data er", data);
+
+  produkt.innerHTML = `<img src="https://kea-alt-del.dk/t7/images/webp/1000/${id}.webp" alt="Sahara Team India Fanwear Round Neck Jersey">
+ <h1>${data.productdisplayname}</h1>
+ <h2> Brand: ${data.brandname} </h2>
+ <p class= "productPrice"><span class="bold">Pris:</span> ${data.price} </p>
+ <p> Kategori: ${data.category} </p>
+ <p> Underkategori: ${data.subcategory} </p>
+ <p> Farve: ${data.basecolour} </p>
+ <p> Årstid: ${data.season} </p>
+ <p> Køn: ${data.gender} </p>
+ 
+
+
+ 
+`;
+}
